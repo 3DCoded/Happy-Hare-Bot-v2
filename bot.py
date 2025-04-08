8f2fdd88da6d77dd46b6fb8c5eb4689fbceb7fe6
3DCoded <69043333+3DCoded@users.noreply.github.com>
2025-04-08 11:38:13 -0400

    feat: Reaction roles

--- bot.py
+++ bot.py
@@ -31,9 +31,11 @@ Other features:
 
 import os
 import random
+import json
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands, tasks
+from discord.utils import get as dget
 
 # Retrieve sensitive information from file
 load_dotenv()
@@ -61,12 +63,25 @@ ADMIN_USERIDS = list(map(int, os.getenv('ADMIN_USERIDS').split(',')))
 UI_CHANNEL_ID = os.getenv('UI_CHANNEL_ID')
 LANDING_CHANNELID = os.getenv('LANDING_CHANNELID')
 
+if not os.path.exists('messages.txt'):
+    with open('messages.txt', 'w+') as file:
+        file.write('')
+
 MMUS = '3DChameleon🦎,3MS,Box Turtle🐢,ERCF🥕,Night Owl🦉,Pico MMU,QuattroBox,Tradrack'.strip().split(',')
 
 intents = discord.Intents.default()
 intents.messages = True  # Read messages
+intents.reactions = True
+intents.members = True
+intents.guild_reactions = True
 intents.message_content = True  # Read message content
+intents.guild_messages = True
 bot = commands.Bot(command_prefix='/', intents=intents)
+
+def intenv(name):
+    return int(os.getenv(name))
+
+ROLES = {}
 
 # Code text
 CODE_TEXT = '''When posting configs or logs, please surround with code fences (\`\`\`) so that Discord formats them correctly. Example:
@@ -98,6 +113,52 @@ async def sync(ctx):
 async def tell_code(ctx, arg: discord.Member):
     await ctx.response.send_message(f'''{arg.mention}{CODE_TEXT}''')
 
+@bot.event
+async def on_raw_reaction_add(payload):
+    # Skip bots
+    if payload.user_id == bot.user.id:
+        return
+
+    # Fetch the user (works even if they're not cached)
+    user = await bot.fetch_user(payload.user_id)
+
+    with open('messages.txt') as file:
+        if str(payload.message_id) not in file.read():
+            return
+    emoji_label = payload.emoji.name
+    for role_name, info in ROLES.items():
+        if info['emoji'] == emoji_label or getattr(info['emoji'], 'name', None) == emoji_label:
+            guild = bot.get_guild(payload.guild_id)
+            role = guild.get_role(info['roleid'])
+            print(role.name)
+            await payload.member.add_roles(role)
+            print(f'Added {user.display_name} to {role_name}')
+            await user.send(f'You have successfully subscribed to {role.name}.')
+            break
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    # Skip bots
+    if payload.user_id == bot.user.id:
+        return
+
+    # Fetch the user (works even if they're not cached)
+    with open('messages.txt') as file:
+        if str(payload.message_id) not in file.read():
+            return
+    user = await bot.fetch_user(payload.user_id)
+    emoji_label = payload.emoji.name
+    for role_name, info in ROLES.items():
+        if info['emoji'] == emoji_label or getattr(info['emoji'], 'name', None) == emoji_label:
+            guild = bot.get_guild(payload.guild_id)
+            member = await guild.fetch_member(user.id)
+            role = guild.get_role(info['roleid'])
+            print(role.name)
+            await member.remove_roles(role)
+            print(f'Removed {member.display_name} from {role_name}')
+            await user.send(f'You have successfully unsubscribed from {role.name}.')
+            break
+
 # Event: Message received
 @bot.event
 async def on_message(message):
@@ -126,6 +187,20 @@ async def on_message(message):
     elif message.content.startswith('!ui'):
         await message.delete()
         await send_ui_msg(message.channel)
+    # !roles command
+    elif message.content.startswith('!roles') and message.author.id in ADMIN_USERIDS:
+        await message.delete()
+        if len(message.content.split()) > 1:
+            channel_id = int(message.content.split()[1][2:-1])
+            channel = await bot.fetch_channel(channel_id)
+        else:
+            channel = message.channel
+        msg = await channel.send('React to this message to subscribe to notifications for your MMU!')
+        with open('messages.txt', 'a') as file:
+            file.write(f'\n{msg.id}')
+        for role_name in ROLES:
+            emoji = ROLES[role_name]['emoji']
+            await msg.add_reaction(emoji)
     # DISABLED: Messages containing "how" AND "?" get a nice message.
     # elif ('how' in message.content.lower() and '?' in message.content) and str(message.channel.id) == str(UI_CHANNEL_ID):
     #     await message.reply("Just check the pinned messages. :smiley:", silent=False)
@@ -135,8 +210,47 @@ async def on_message(message):
 # Event: Bot is ready
 @bot.event
 async def on_ready():
+    global ROLES
     print(f'{bot.user} has connected to Discord! {bot.guilds[0].name}')
     # await bot.change_presence(activity=discord.Game(name='Printing with an MMU...'))
+    ROLES = {
+        '3DChameleon': {
+            'emoji': bot.get_emoji(intenv('3DCHAMELEON_EMOJI')),
+            'roleid': intenv('3DCHAMELEON_ROLE')
+        },
+        '3MS': {
+            'emoji': bot.get_emoji(intenv('3MS_EMOJI')),
+            'roleid': intenv('3MS_ROLE')
+        },
+        'Angry Beaver': {
+            'emoji': '🦫',
+            'roleid': intenv('ANGRYBEAVER_ROLE')
+        },
+        'Box Turtle': {
+            'emoji': '🐢',
+            'roleid': intenv('BOXTURTLE_ROLE')
+        },
+        'ERCF': {
+            'emoji': '🥕',
+            'roleid': intenv('ERCF_ROLE')
+        },
+        'Night Owl': {
+            'emoji': '🦉',
+            'roleid': intenv('NIGHTOWL_ROLE')
+        },
+        'Pico MMU': {
+            'emoji': bot.get_emoji(intenv('PICO_EMOJI')),
+            'roleid': intenv('PICO_ROLE')
+        },
+        'QuattroBox': {
+            'emoji': bot.get_emoji(intenv('QUATTROBOX_EMOJI')),
+            'roleid': intenv('QUATTROBOX_ROLE')
+        },
+        'Tradrack': {
+            'emoji': bot.get_emoji(intenv('TRADRACK_EMOJI')),
+            'roleid': intenv('TRADRACK_ROLE')
+        },
+    }
     changeStatus.start()
 
 
